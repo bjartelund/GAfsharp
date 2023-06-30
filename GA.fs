91519b2e57581388cb04843b9c39707a590b88a0
Bjarte Aarmo Lund <bjarte.lund@gmail.com>
2023-06-30 23:55:57 +0200

    Delete GA.fs

--- GA.fs
+++
@@ -1,7 +0,0 @@
-type Weight = int
-type Price = int
-type Quantity = int
-type Thing = {Weight: Weight; Price: Price ; Quantity: Quantity}
-type Banana = Thing
-
-let banan : Banana = { Weight = 1; Price = 2; Quantity = 1}
